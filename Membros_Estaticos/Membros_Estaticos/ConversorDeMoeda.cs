15838e3c08800cc34eb46054303a8f84034e9d47
cristiano campos <cristiano.souzac@hotmail.com.br>
2021-02-24 19:51:34 -0300

    Membros Estaticos

---
+++ Membros_Estaticos/Membros_Estaticos/ConversorDeMoeda.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Membros_Estaticos
+{
+    class ConversorDeMoeda
+    {
+        public static double IOF = 6;
+
+        public static double ValorPagar(double dolar, double cotacao)
+        {
+            double total = dolar * cotacao;
+            return total + total * 6 / 100;
+        }
+    }
+}
---
+++ Membros_Estaticos/Membros_Estaticos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Membros_Estaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Qual é a cotação do dólar? ");
+            double contacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoeda.ValorPagar(dolar, contacao);
+
+            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
