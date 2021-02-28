1bde2207b50077f7aac8e828731f734e861b254c
cristiano campos <cristiano.souzac@hotmail.com.br>
2021-02-28 12:28:15 -0300

    Construtores, this, sobrecarga, encapsulamento

---
+++ Construtores_This_Sobrecarga_Encapsulamento/Construtores_This_Sobrecarga_Encapsulamento/Conta.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+
+namespace Construtores_This_Sobrecarga_Encapsulamento
+{
+    class Conta
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public Conta(int numero, string titular, double saldo) : this(numero, titular)
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", "
+                + "Titular: "
+                + Titular
+                + ", "
+                + "Saldo: R$ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
